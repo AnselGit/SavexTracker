be3cc73c232450de58eb4686c3622d8c8da21ca9
John Ansel Doton <johnanseldoton@gmail.com>
2025-07-13 15:49:40 +0800

    bargraph works

--- forms/Form1.cs
+++ forms/Form1.cs
@@ -34,6 +34,8 @@ namespace SavexTracker
         {
             ChartBuilder.BuildLineGraph(pnlLine);
             DonutChartBuilder.Build(pnlPie);
+            BarGraphBuilder.BuildBarGraph(pnlBar);
+
 
             UpdateTotalLabels();            
             EnsureDatabaseAndTable();
